55848708b711c15ecc56cc0d7855729314ba99d0
garymalaysia <garytsai@live.com>
2017-04-13 13:26:47 -0400

    clone

--- exec.py
+++ exec.py
@@ -3,7 +3,7 @@ import shutil, os
 from time import strftime
 import time, threading
 import calendar
-#from autoEmail import email
+from autoEmail import email
 from backup import back_up
 from add_column import add_column
 from incre_col import increase_col
@@ -26,7 +26,7 @@ def exe():
 		if now == backup1 or now == backup2 or now == backup3 or now == backup4 or now == backup5:
 			back_up()
 		if today == end_this_mon and now == time_to_exe:
-			#email()
+			email()
 			add_column()
 			increase_col()
 			
